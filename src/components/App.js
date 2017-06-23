7b2c9ede6294c2f4047196ad92c7def600b76c17
nillk <hyunjoo88.kim@gmail.com>
2017-06-24 01:09:00 +0900

    test counter example

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Counter from './Counter';
+import Buttons from './Buttons';
+import Option from './Option';
 
 class App extends React.Component {
   render(){
     return (
-      <h1>Hello React Skeleton</h1>
+      <div style={ {textAlign: 'center'} }>
+        <Counter/>
+        <Option/>
+        <Buttons/>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider  } from 'react-redux';
 import App from './components/App';
+import counterApp from './reducers';
 
+const store = createStore(counterApp);
 const rootElement = document.getElementById('content');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>,
+    rootElement
+);
